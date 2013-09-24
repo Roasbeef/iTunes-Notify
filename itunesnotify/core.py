f2f18766b663f853d69db1f01b77372a6f0dbc46
Olaoluwa Osuntokun <laolu32@gmail.com>
2013-09-24 00:47:47 -0500

    Ironing out some bugs, temp debug statements

--- itunesnotify/core.py
+++ itunesnotify/core.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python
+#import gevent
 import subprocess
 import os
 import sys
 import re
-import pykka
 
 from collections import namedtuple
 from gntp.notifier import GrowlNotifier
+from pykka.gevent import GeventActor
 
 
 __version__ = (0, 0, 1, 0)
+
 PID_PATH = os.path.join(os.path.dirname(__file__), "pid.txt")
 
 
 def write_pid(pid):
     """ Writes process pid to file for stopping execution later """
+    print 'Writing to pid file'
     with open(PID_PATH, 'w+') as f:
         f.write(str(pid))
 
@@ -23,11 +26,13 @@ def already_running():
     """ Return true if an instance of iTunes-Notify is currently running """
     with open(PID_PATH, 'r') as f:
         pid = f.read()
+        # return not pid.strip()
         return pid != ' ' and pid != ''
 
 
 def begin_notifications():
     """ Start iTunes-Notify, unless an instance is currently running """
+    print 'Starting Notifications'
     if already_running():
         print 'iTunes-Notify is already running'
         sys.exit(0)
@@ -47,6 +52,7 @@ def end_notifications():
     """ Stop iTunes-Notify if one is running
         otherwise exits with a message
     """
+    print 'Trying to stop notifications'
     with open(PID_PATH, 'r+') as f:
         pid = f.read()
         #blank file so program not running
@@ -54,6 +60,8 @@ def end_notifications():
             print 'iTunes-Notify is not currently running'
             sys.exit(0)
         #kill process
+        # to block: subprocess.call(["kill", "-9", "%s" % pid]).wait()
+        # TODO(laolu): Actually check for an error here
         subprocess.call(["kill", "-9", "%s" % pid])
         print 'iTunes-Notify successfully stopped'
 
@@ -69,6 +77,7 @@ class iTunes(object):
         """ Returns true if there is an iTunes (not the pesky iTunes helper)
             process running, false otherwise
         """
+        print 'Checking if open'
         ps_cmd = ["ps", "axo", "pid,command"]
         p = subprocess.Popen(ps_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         processes = p.communicate()[0]
@@ -77,6 +86,7 @@ class iTunes(object):
     @staticmethod
     def current_status():
         """ Returns current status of itunes """
+        print 'Getting status of itunes'
         return subprocess.check_output(["osascript", "-e", 'tell application "iTunes"'
                                         'to player state as string']).strip()
 
@@ -85,15 +95,17 @@ class iTunes(object):
         """ Returns name, artist, and album of
             the current song currenly playing
         """
+        print 'Grabbing song details'
         return subprocess.check_output(["osascript", "-e", 'tell application "iTunes"'
                                        'to name of current track'
                                        '&"#"& artist of current track'
                                        '&"#"& album of current track as string']).strip().split('#')
 
 
-class iTunesListenerActor(pykka.gevent.GeventActor):
+class iTunesListenerActor(GeventActor):
     def __init__(self, notifier):
         super(iTunesListenerActor, self).__init__()
+        print 'Init listener actor'
         self.notifier = notifier
         self.itunes = iTunes()
         self.last_track_title = ''
@@ -101,33 +113,39 @@ class iTunesListenerActor(pykka.gevent.GeventActor):
 
     def listen(self, message):
         # TODO: Find a better way handling this
+        print 'Starting listen loop'
         while True:
             try:
                 if self.itunes.is_open() and self.itunes.current_status() == 'playing':
                     title, artist, album = self.iTunes.current_song_details()
                     # don't reply with the song sent last
                     if title.get() != self.last_track_title:
+                        print 'new track'
                         s = self.new_track(title.get(), artist.get(), album.get())
                         # send the new song over to the notifier actor
+                        print 'Listener sending off new track'
                         self.notifier.tell({'new_track': s})
+                    else:
+                        print 'Seen track before'
+                        continue
             except:  # iTunes might be open/closed for a split second and cause an error
                 pass
 
 
-class iTunesNotifierActor(pykka.gevent.GeventActor, GrowlNotifier):
+class iTunesNotifierActor(GeventActor, GrowlNotifier):
     def __init__(self):
         # register growl app with relevant information
-        super(iTunesNotifierActor, self).__init__(applicationName="iTunes",
-                                                  notifications=["Current Song"],
-                                                  defaultNotifications=["Current Song"])
-        self.register()
+        self.growl = GrowlNotifier(applicationName="iTunes",
+                                   notifications=["Current Song"],
+                                   defaultNotifications=["Current Song"])
+        self.growl.register()
         # initialize actor
         super(iTunesNotifierActor, self).__init__()
 
     def on_receive(self, message):
         track = message['new_track']
-        self.notify(noteType="Current Track", title=track.name,
-                    description='by {} from {}'.format(track.artist, track.album))
+        self.growl.notify(noteType="Current Track", title=track.name,
+                          description='by {} from {}'.format(track.artist, track.album))
 
     def on_start(self):
         print 'iTunes-Notify is now running.'
